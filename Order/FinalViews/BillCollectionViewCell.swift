a0e94fb579aa7c5a060d43e5bbc15cd7bf4983ae
渡邉　博 <hiroshi@MacBook.local>
2019-07-21 15:55:00 +0900

    add BillCollectionViewCell constraints

--- Order/FinalViews/BillCollectionViewCell.swift
+++ Order/FinalViews/BillCollectionViewCell.swift
@@ -22,6 +22,8 @@ class BillCollectionViewCell: UICollectionViewCell {
     
     func setupViews() {
         
+        backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        
         foodImageView.backgroundColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
         foodImageView.contentMode = .scaleAspectFill
         foodImageView.clipsToBounds = true
@@ -29,17 +31,36 @@ class BillCollectionViewCell: UICollectionViewCell {
         addSubview(foodImageView)
         foodImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 0).isActive = true
         foodImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 0).isActive = true
+        foodImageView.widthAnchor.constraint(equalTo: self.heightAnchor, multiplier: 1).isActive = true
+        foodImageView.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 1).isActive = true
         foodImageView.translatesAutoresizingMaskIntoConstraints = false
         
         
         foodNameLabel.backgroundColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
         addSubview(foodNameLabel)
+        foodNameLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 0).isActive = true
+        foodNameLabel.leftAnchor.constraint(equalTo: foodImageView.rightAnchor, constant: 0).isActive = true
+        foodNameLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: 0).isActive = true
+        foodNameLabel.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.5).isActive = true
+        foodNameLabel.translatesAutoresizingMaskIntoConstraints = false
         
         foodPriceLabel.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
         addSubview(foodPriceLabel)
+        foodPriceLabel.topAnchor.constraint(equalTo: foodNameLabel.bottomAnchor, constant: 0).isActive = true
+        foodPriceLabel.leftAnchor.constraint(equalTo: foodImageView.rightAnchor, constant: 0).isActive = true
+//        foodPriceLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: 0).isActive = true
+        foodPriceLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0).isActive = true
+        foodPriceLabel.widthAnchor.constraint(equalTo: foodNameLabel.widthAnchor, multiplier: 0.5).isActive = true
+        foodPriceLabel.translatesAutoresizingMaskIntoConstraints = false
         
         foodNumLabel.backgroundColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
         addSubview(foodNumLabel)
+        foodNumLabel.topAnchor.constraint(equalTo: foodNameLabel.bottomAnchor, constant: 0).isActive = true
+        foodNumLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: 0).isActive = true
+        foodNumLabel.leftAnchor.constraint(equalTo: foodPriceLabel.rightAnchor, constant: 0).isActive = true
+        foodNumLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0).isActive = true
+        foodNumLabel.translatesAutoresizingMaskIntoConstraints = false
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
